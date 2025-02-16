6315818e425d634ee26a17062a48beff628507bf
maryammrg <maryamelouadaa@gamil.com>
2025-02-16 21:52:50 +0100

    create servlet

--- src/main/java/com/example/usermannager/controller/CreateServlet.java
+++ src/main/java/com/example/usermannager/controller/CreateServlet.java
@@ -2,4 +2,24 @@ package com.example.usermannager.controller;
 
 public class CreateServlet {
 
+private UserDAO userDAO ;
+public void init() {
+    userDAO = new UserDAO();
+}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("createPersonForm.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String post = request.getParameter("post");
+        double salaire = Double.parseDouble(request.getParameter("salaire"));
+        User user = new User(name, email, phone, post, salaire);
+        userDAO.insertUser(user);
+        response.sendRedirect("read");
+    }
+
 }
